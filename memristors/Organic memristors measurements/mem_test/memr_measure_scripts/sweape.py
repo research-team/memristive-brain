848a962c844a54bf6cea25b7959becabc60ac7d4
M.Aider <ideeer4@gmail.com>
2019-07-01 18:53:28 +0300

    UPDATE syntax for queries

--- memristors/Organic memristors measurements/mem_test/memr_measure_scripts/sweape.py
+++ memristors/Organic memristors measurements/mem_test/memr_measure_scripts/sweape.py
@@ -1,5 +1,4 @@
 import visa
-
 from equipment import KeithlySmu
 
 if __name__ == '__main__':
@@ -10,9 +9,19 @@ if __name__ == '__main__':
     K.default_settings()
     K.resetToDefaultState()
     K.flushDataToFile("reset_memristor_{}.csv".format(EXPERIMENT))
-
+    start = 0
+    stop = 1
+    points = 1000
+    delay = 0
+    count = 1
+    rangeType = 'FIXED'
+    failAbort = 'OFF'
+    dual = 'ON'
+    bufferName = 'defbuffer1'
+    command = f"SOUR:SWE:VOLT:LIN {start}, {stop}, {points}, {delay}," \
+              f" {count}, {rangeType}, {failAbort}, {dual}, {bufferName}"
     K.query(
-        "SOUR:SWE:VOLT:LIN {start}, {stop}, {points}, {delay}, {count}, {rangeType}, {failAbort}, {dual}, {bufferName}"
-        .format(0, 1, 1000, 0, 1, 'FIXED', 'OFF', 'ON', 'defbuffer1'))
+        command
+    )
     K.flushDataToFile('all_res_{}.csv'.format(EXPERIMENT))
     K.close()
