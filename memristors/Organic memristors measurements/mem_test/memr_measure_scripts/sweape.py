38b767127412739ceba706663e560404360aa007
AmirGiz <51257858+AmirGiz@users.noreply.github.com>
2019-07-10 16:21:08 +0300

    Лесенка 4 мемристор

--- memristors/Organic memristors measurements/mem_test/memr_measure_scripts/sweape.py
+++ memristors/Organic memristors measurements/mem_test/memr_measure_scripts/sweape.py
@@ -2,14 +2,14 @@ import visa
 from equipment import KeithlySmu
 
 if __name__ == '__main__':
-    EXPERIMENT, HOURS = 2, 2
+    EXPERIMENT, HOURS = 3, 2
     rm = visa.ResourceManager()
     K = KeithlySmu('keithly', rm.list_resources()[0], delay=0.3)
 
     K.default_settings()
     K.resetToDefaultState()
     K.flushDataToFile("reset_memristor_{}.csv".format(EXPERIMENT))
-    start = 0
+    start = -0.2
     stop = 1
     points = 75000 # 37600 - one hour test 75000 - two hour
     delay = 0
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     bufferName = '\"defbuffer1\"'
     command = f"SOUR:SWE:VOLT:LIN {start}, {stop}, {points}, {delay}, {count}, {rangeType}, {failAbort}, {dual}, {bufferName}"
     K.write(
-        command
+      command
     )
     K.write('INIT')
     # Use this commands manualy
