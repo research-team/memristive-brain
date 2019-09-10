82803471c13ac0c649e113d72fc040453cd6f64e
AmirGiz <51257858+AmirGiz@users.noreply.github.com>
2019-09-10 17:44:36 +0300

    05

--- memristors/Organic memristors measurements/mem_test/memr_measure_scripts/newK.py
+++ memristors/Organic memristors measurements/mem_test/memr_measure_scripts/newK.py
@@ -7,7 +7,7 @@ from equipment import KeithlySmu
 
 for PART in range(1, 4):
     if __name__ == '__main__':
-        EXPERIMENT, STAGE = "03", 1
+        EXPERIMENT, STAGE = "05", 2
         rm = visa.ResourceManager()
         K = KeithlySmu('keithly', rm.list_resources()[0], delay=0.3)
         K.default_settings()
@@ -33,7 +33,7 @@ for PART in range(1, 4):
 
             K.write("INIT")
             time.sleep(delay + 1)
-            K.flushDataToFile("res{}_{}_{}_{}.csv".format(EXPERIMENT, PART, idx, s))
+            K.flushDataToFile("res{}_{}_{}_{}_{}.csv".format(EXPERIMENT, STAGE, PART, idx, s))
             time.sleep(10)
             size = int(K.query("TRACe:ACTual? \"defbuffer1\"")[0])
             d = K.queryContainer("TRACe:DATA? 1,{}, \"defbuffer1\",SOUR,READ,REL".format(size))
@@ -47,8 +47,8 @@ for PART in range(1, 4):
             for idx, s in enumerate(steps):
                 data = Method(idx, s, EXPERIMENT, PART, delay, data)
 
-            name_from = f"res{EXPERIMENT}_1_20_0.csv"
-            name_to = f"res{EXPERIMENT}_2_0_0.csv"
+            name_from = f"res{EXPERIMENT}_{STAGE}_1_20_0.csv"
+            name_to = f"res{EXPERIMENT}_{STAGE}_2_0_0.csv"
             copyfile(name_from, name_to)
         elif PART == 2:
             steps = ["-0.1", "-0.2", "-0.3", "-0.4", "-0.5", "-0.6", "-0.7", "-0.8", "-0.9", "-1"]
